0c2615c0b1b781b597b0a97010c62640820efbca
Hugues Ebert M <huguesbert17@gmail.com>
2023-06-12 17:51:50 +0000

    Addding css and create card template

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const signs = ["♠", "♦", "♣", "♥"],
+  cards = ["K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2", "A"],
+  jacks = [],
+  colors = ["red", "black"];
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".game-card").innerHTML = createCard();
 };
+
+function createCard() {
+  return cardTemplate(random(colors), random(signs), random(cards));
+}
+
+function cardTemplate(color, sign, number) {
+  let card = '<div class="corner top">';
+  card += `<span class="card-number">${number}</span>`;
+  card += `<span class="sign">${sign}</span>`;
+  card += "</div>";
+
+  card += `<div class="middle"><span>${sign}</span></div>`;
+
+  card += '<div class="corner bottom">';
+  card += `<span class="card-number">${number}</span>`;
+  card += `<span class="sign">${sign}</span>`;
+  card += "</div>";
+  return `<div class="card-${color}">${card}</div>`;
+}
+
+function random(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
